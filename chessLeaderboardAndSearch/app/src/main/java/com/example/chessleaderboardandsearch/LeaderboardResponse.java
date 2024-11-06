fb0eb6c60d474bdb08a8e038995ee15dd093a601
VaxM <vinhtt.22bi13474@usth.edu.vn>
2024-11-06 15:57:44 +0700

    chess app leaderboard, search function and player profile done

---
+++ chessLeaderboardAndSearch/app/src/main/java/com/example/chessleaderboardandsearch/LeaderboardResponse.java
@@ -0,0 +1,29 @@
+// LeaderboardResponse.java
+package com.example.chessleaderboardandsearch;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class LeaderboardResponse {
+    @SerializedName("daily")
+    private List<Player> daily;
+
+    @SerializedName("live_rapid")
+    private List<Player> liveRapid;
+
+    @SerializedName("live_blitz")
+    private List<Player> liveBlitz;
+
+    // Getters
+    public List<Player> getDaily() {
+        return daily;
+    }
+
+    public List<Player> getLiveRapid() {
+        return liveRapid;
+    }
+
+    public List<Player> getLiveBlitz() {
+        return liveBlitz;
+    }
+}
