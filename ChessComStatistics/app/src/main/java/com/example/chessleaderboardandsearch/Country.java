35da113e7862bbc30c24290d4ecbbe3e0972b87c
Tran Anh Vu <tav04092004@gmail.com>
2024-11-09 16:09:55 +0700

    Merge everyone code and make the final product

--- ChessComStatistics/app/src/main/java/com/example/chessleaderboardandsearch/Country.java
+++
@@ -1,9 +0,0 @@
-package com.example.chessleaderboardandsearch;
-
-public class Country {
-    private String code;
-    private String name;
-
-    public String getCode() { return code; }
-    public String getName() { return name; }
-}
